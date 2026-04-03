99a448fdfb3c2b0b758d4851f527edef3e3eb941
CGH0S7 <776459475@qq.com>
2026-04-03 18:43:19 +0800

    Pin Flutter version and fix split APK version codes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.OutputFile
+import com.android.build.gradle.internal.api.ApkVariantOutputImpl
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -33,6 +36,20 @@ android {
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
+    }
+}
+
+val abiCodes = mapOf(
+    "armeabi-v7a" to 1,
+    "arm64-v8a" to 2,
+    "x86_64" to 3,
+)
+
+android.applicationVariants.configureEach {
+    val variantVersionCode = versionCode
+    outputs.configureEach {
+        val abi = abiCodes[filters.find { it.filterType == OutputFile.ABI }?.identifier] ?: 0
+        (this as ApkVariantOutputImpl).versionCodeOverride = variantVersionCode * 10 + abi
     }
 }
 
