85599841c0495cf3b466f57d5811f59766aa45bc
CGH0S7 <776459475@qq.com>
2026-04-23 10:55:09 +0800

    android: shrink release APK classes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -55,8 +55,12 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
         }
     }
 
---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,2 @@
+# Keep this file intentionally minimal. Release shrinking is enabled to strip
+# unused Flutter deferred-components Play Core classes that F-Droid blocks.
